94686a91f4fda0aea1d891d2c8cd7ef360c0315e
jypkr <wodusqkr1210@gmail.com>
2021-10-20 16:25:28 -0700

    user only can use website when user is logged in

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,6 +6,7 @@ import {
 import Auth from './Pages/Auth'
 import Home from './Pages/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import History from './Pages/History'
 import Leaderboard from './Pages/Leaderboard';
 import AboutUs from './Pages/AboutUs';
@@ -16,8 +17,9 @@ function App() {
       <div>
         <Switch>
           <Route exact path='/'>
-          <Home />
             <Header />
+            <Home />
+            <Footer />
           </Route>
           <Route path='/signIn'>
             <Auth />
