57fcb46419c9d062281a6ca761c2c47232cb144a
jypkr <wodusqkr1210@gmail.com>
2021-10-20 16:12:17 -0700

    History display updated

--- Client/src/components/DropdownWeekNumForm/DropdownWeeknumForm.js
+++ Client/src/components/DropdownWeekNumForm/DropdownWeeknumForm.js
@@ -2,10 +2,14 @@ import Dropdown from 'react-bootstrap/Dropdown'
 
 const DropdownWeeknumForm = ({
 	weekNum,
-	getfunction
+	getfunction,
+	getfunction2
 }) => {
 	return (
-		<Dropdown.Item onClick={() => getfunction(weekNum)}>{weekNum}</Dropdown.Item>
+		<Dropdown.Item onClick={() => {
+			getfunction(weekNum)
+			if (getfunction2 !== undefined) { getfunction2(weekNum) }
+		}}>{weekNum}</Dropdown.Item>
 	)
 }
 
