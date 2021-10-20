258703b74e0d49d7371bd25e307c9f51e15454d0
Lan714 <87465185+Lan714@users.noreply.github.com>
2021-10-20 15:17:30 -0700

    components transferred

---
+++ Client/components/SignInForm/SignInForm.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import UserAPI from '../../utils/UserAPI'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import './SignIn.css'
+
+
+const SignInForm = () => {
+	const [userState, setUserState] = useState({
+		username: '',
+		password: ''
+	})
+
+	const handleInputChange = ({ target: { name, value } }) => setUserState({ ...userState, [name]: value })
+
+	const handleLoginUser = event => {
+		event.preventDefault()
+		UserAPI.login(userState)
+			.then(({ data: token }) => {
+				if (token) {
+					localStorage.setItem('token', token)
+					setUserState({ ...userState, name: '', email: '', username: '', password: '' })
+					window.location = '/'
+				}
+				else {
+					alert('User unable to login idk why')
+				}
+			})
+			.catch(err => console.error(err))
+	}
+
+	return (
+		<Form>
+			<Form.Group className="mb-3" controlId="username">
+				<Form.Label>Username</Form.Label>
+				<Form.Control
+					type="text"
+					placeholder="Enter your username"
+					name="username"
+					value={userState.username}
+					onChange={handleInputChange} />
+			</Form.Group>
+			<Form.Group className="mb-3" controlId="password">
+				<Form.Label>Password</Form.Label>
+				<Form.Control
+					type="password"
+					placeholder="Enter your password"
+					name="password"
+					value={userState.password}
+					onChange={handleInputChange} />
+			</Form.Group>
+			<Button
+				variant="primary"
+				type="submit"
+				onClick={handleLoginUser} >
+				Sign In
+			</Button>
+		</Form>
+	)
+}
+
+export default SignInForm
