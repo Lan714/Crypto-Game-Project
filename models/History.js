000546492be988cd53ed238105e58645e323e21f
Lan714 <87465185+Lan714@users.noreply.github.com>
2021-10-20 15:10:03 -0700

    models added

---
+++ models/History.js
@@ -0,0 +1,27 @@
+const { model, Schema } = require('mongoose')
+
+const History = new Schema({
+  weekNumber: Number,
+  cash_balance: String,
+  crypto_balace: [{
+    crypto_name: String,
+    dollar_value: String
+  }],
+  transaction: [{
+    date: String,
+    crypto_name: String,
+    side: String,
+    price: String,
+    amount: String,
+    total: String
+  }],
+  profit: Number,
+  username: String,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+module.exports = model('History', History)
+
