6caba0a50b2f3d35fd58c1afea976668f001a89d
Lan714 <87465185+Lan714@users.noreply.github.com>
2021-10-20 15:07:59 -0700

    utils added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Auth from './Pages/Auth'
+import Home from './Pages/Home'
+import Header from './components/Header'
+import History from './Pages/History'
+import Leaderboard from './Pages/Leaderboard';
+import AboutUs from './Pages/AboutUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/'>
+          <Home />
+            <Header />
+          </Route>
+          <Route path='/signIn'>
+            <Auth />
+          </Route>
+          <Route path='/history'>
+            <History />
+          </Route>
+          <Route path='/Leaderboard'>
+            <Leaderboard />
+          </Route>
+          <Route path='/AboutUs'>
+            <AboutUs />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
