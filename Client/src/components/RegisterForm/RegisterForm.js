07fae8c1bba9441e84ca4b5d94b2ed6e87eafac3
Samantha Mack <heaulin@gmail.com>
2021-10-21 02:09:03 -0700

    added css link

--- Client/src/components/RegisterForm/RegisterForm.js
+++ Client/src/components/RegisterForm/RegisterForm.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import UserAPI from '../../utils/UserAPI'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-
+import './Register.css'
 
 
 const RegisterForm = () => {
