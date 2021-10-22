00fa3f7f2805865dcf3d74da1b67921df5c3fab1
Samantha Mack <heaulin@gmail.com>
2021-10-22 03:04:11 -0700

    changed css for footer and added footers to  pages

--- Client/src/components/Footer/Footer.js
+++ Client/src/components/Footer/Footer.js
@@ -1,24 +1,21 @@
 import React from 'react'
-import Card from 'react-bootstrap/card'
 import './Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope, faUsers, faAddressBook, faInfoCircle} from '@fortawesome/free-solid-svg-icons'
+
 const Footer = () => {
   return (
-<Card>
-  <Card.Body>
-      <div className="mt-5 pt-5 pb-5 footer">
+      <div className="footer">
         <div className="container">
           <div className="row">
-            <div className="col-lg-5 col-xs-12 info">
-              <h4><FontAwesomeIcon icon={faInfoCircle} className="info" />About CryptoMaster</h4>
-              <h6 className="pr-5">
-                A gaming app that allows the users to practice their skills in crypto currency exchange. This app was developed by 6 coding boot camp students from UC Irvine. Happy Gaming!  
-              </h6>
+            <div className="col-md-3 col-sm-6 info">
+              <h4><FontAwesomeIcon icon={faInfoCircle} className="icon" />About CryptoMaster</h4>
+              <p className="text"> A gaming app that allows the users to practice their skills in crypto currency exchange. This app was developed by 6 coding boot camp students from UC Irvine. Happy Gaming! 
+              </p>
             </div>
-            <div className="col-lg-3 col-xs-12 links">
-              <h4 className="mt-lg-0 mt-sm-3"><FontAwesomeIcon icon={faUsers} className="phone" />About us</h4>
-              <ul className="m-0 p-0">
+            <div className="col-md-3 col-sm-6 links">
+              <h4 className="mt-lg-0 mt-sm-3"><FontAwesomeIcon icon={faUsers} className="icon" />About us</h4>
+              <ul className="list-unstyled">
                 <li> <a href="https://github.com/jypkr">Jaeyeon Park</a></li>
                 <li> <a href="https://github.com/Lan714">Lan Bui</a></li>
                 <li> <a href="https://github.com/jjbenson13">Jasmine Benson</a></li>
@@ -27,21 +24,23 @@ const Footer = () => {
                 <li> <a href="https://github.com/Heaulin">Samantha Mack</a></li>
               </ul>
             </div>
-            <div className="col-lg-4 col-xs-12 Contact">
-              <h4 className="mt-lg-0 mt-sm-4"><FontAwesomeIcon icon={faAddressBook} className="address" />Contact us</h4>
-              <p class="mb-0"><FontAwesomeIcon icon={faPhone} className="phone" />(541) 754-3010</p>
-              <p><FontAwesomeIcon icon={faEnvelope} className="mail" />cryptomaster@gmail.com</p>
+            <div className="col-md-3 col-sm-6 Contact">
+              <h4 className="mt-lg-0 mt-sm-4">
+                <FontAwesomeIcon icon={faAddressBook} className="icon" />Contact us</h4>
+              <p className="mb-0">
+                <FontAwesomeIcon icon={faPhone} className="icon" />(541) 754-3010</p>
+              <p>
+                <FontAwesomeIcon icon={faEnvelope} className="icon" />cryptomaster@gmail.com
+                </p>
             </div>
           </div>
-          <div class="row mt-5">
-    <div class="col copyright">
-      <p class=""><small class="text-white-50">© 2021. All Rights Reserved.</small></p>
+          <div className="row mt-1">
+    <div className="col copyright">
+      <p className=""><small className="text-white-50">© 2021. All Rights Reserved.</small></p>
     </div>
   </div>
         </div>
       </div>
-  </Card.Body>
-</Card>
   )
 }
 export default Footer
--- Client/src/components/Footer2/Footer2.js
+++ Client/src/components/Footer2/Footer2.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Footer2.css'
 
-const CopyRightFooter = () => {
+const Footer2 = () => {
 
   return (
     <div className="container">
-      <div class="row mt-5">
-    <div class="col copyright">
-      <p class=""><small class="text-grey-50">© 2021. All Rights Reserved.</small></p>
+      <div className="row mt-5">
+    <div className="col copyright">
+      <p className=""><small className="text-grey-50">© 2021. All Rights Reserved.</small></p>
     </div>
   </div>
   </div>
   )
 }
-export default CopyRightFooter
+export default Footer2
