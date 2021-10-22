65fc1667eebf2fc0bcdbb8c92fdc0c6d4dd7eb95
Samantha Mack <heaulin@gmail.com>
2021-10-21 22:37:42 -0700

    added copyright footer

---
+++ Client/src/components/CopyrightFooter/opyRightFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CopyRightFooter = () => {
+
+  return (
+    <div className="container">
+      <div class="row mt-5">
+    <div class="col copyright">
+      <p class=""><small class="text-grey-50">© 2021. All Rights Reserved.</small></p>
+    </div>
+  </div>
+  </div>
+  )
+}
+export default CopyRightFooter
