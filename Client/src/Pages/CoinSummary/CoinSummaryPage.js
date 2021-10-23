054d2ca577aa69479a1f56a9f5bcd4740fee5cb1
Jasmine Benson <jjortega2010@gmail.com>
2021-10-23 14:51:34 -0700

    COin pages

---
+++ Client/src/Pages/CoinSummary/CoinSummaryPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddCoin from '../../components/AddCoin'
+import CoinList from '../../components/CoinList/CoinList'
+import './CoinSummaryPage.css'
+
+const CoinSummaryPage = () => {
+  return (
+    <div className='coinsummary border p-2 rounded mt bg-light'>
+      <AddCoin />
+      <CoinList />
+    </div>
+  )
+}
+
+export default CoinSummaryPage
