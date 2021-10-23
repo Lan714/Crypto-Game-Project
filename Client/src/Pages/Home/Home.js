e41e2a96d552e97b1a2d9a2674f36024c3c70394
jypkr <wodusqkr1210@gmail.com>
2021-10-23 15:25:07 -0700

    123123

--- Client/src/Pages/Home/Home.js
+++ Client/src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import Navbar from '../../components/NavBar'
 import './Home.css'
 import Footer from '../../components/Footer/Footer'
+import CoinSummaryPage from '../CoinSummary/CoinSummaryPage'
 
 const Home = () => {
 	if (!localStorage.getItem('token')) {
