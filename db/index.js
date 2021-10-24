16188170bff65fb879648d5c8b09ac43eaca754a
Lan714 <87465185+Lan714@users.noreply.github.com>
2021-10-24 13:57:14 -0700

    server test

--- db/index.js
+++ db/index.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose')
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://ProjectLeadsTeamUCI:Y61b7bQc38bvgFr3@cryptomaster.a1bki.mongodb.net/CoinDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 module.exports = async function syncDB() { await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/CoinDB') }
