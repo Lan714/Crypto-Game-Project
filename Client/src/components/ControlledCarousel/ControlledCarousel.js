f32ce0df049d4139a962be2323434c1b45206592
Samantha Mack <heaulin@gmail.com>
2021-10-24 16:17:07 -0700

    linked up names in carousel

--- Client/src/components/ControlledCarousel/ControlledCarousel.js
+++ Client/src/components/ControlledCarousel/ControlledCarousel.js
@@ -10,46 +10,45 @@ function ControlledCarousel() {
   return (
     <div id="Title" style={{ display: 'block', width: 500, padding: 30, color:'white' }}>
       <h1>Who We Are</h1>
-      <h3>CryptoMaster is the brainchild of six UCI Full Stack Bootcamp Students</h3>
+      <h3>CryptoMaster is the brainchild of six UCI Full Stack Bootcamp Students. Click on our names to find out more about us.</h3>
       <Carousel center="xs" id="WhoWeAre">
         <Carousel.Item interval={2000}>
           <img className="d-block w-100" src="https://cdn.pixabay.com/photo/2017/12/15/13/51/polynesia-3021072__340.jpg"/>
           <Carousel.Caption>
-            <h3>Jasmine Benson</h3>
+            <h3> <a className="text-white text-decoration-none" href="https://github.com/jjbenson13">Jasmine Benson</a> </h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={2000}>
           <img id="Image"className="d-block w-100" src="https://cdn.pixabay.com/photo/2021/09/20/21/32/lake-6641880__480.jpg"/>
           <Carousel.Caption>
-            <h3>Lan Bui</h3>
+            <h3><a className="text-white text-decoration-none" href="https://github.com/Lan714">Lan Bui</a></h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={2000}>
           <img id="Image" className="d-block w-100" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"/>
           <Carousel.Caption>
-            <h3>Remi Kim</h3>
+            <h3><a className="text-white text-decoration-none" href="https://github.com/remireems">Remi Kim</a></h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={2000}>
           <img id="Image" className="d-block w-100" src={Samantha}/>
           <Carousel.Caption>
-            <h3>Samantha Mack</h3>
+            <h3><a className="text-white text-decoration-none" href="https://github.com/Heaulin">Samantha Mack</a></h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={2000}>
           <img id="Image" className="d-block w-100" src={Jayeon}/>
           <Carousel.Caption>
-            <h3>Jaeyeon Park</h3>
+            <h3><a className="text-white text-decoration-none" href="https://github.com/jypkr">Jaeyeon Park</a></h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={2000}>
           <img id="Image"className="d-block w-100" src={Shannon}/>
           <Carousel.Caption>
-            <h3>Shannon Rayes</h3>
+            <h3><a className="text-white text-decoration-none" href="https://github.com/shannono18">Shannon Rayes</a></h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-
     </div>
   );
 }
