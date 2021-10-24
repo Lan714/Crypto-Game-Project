b67438f7b887a1c43b7e1c7201847081fea67f50
Remi <75871744+remireems@users.noreply.github.com>
2021-10-23 22:08:39 -0700

    added title in home page

--- Client/src/Pages/Home/Home.js
+++ Client/src/Pages/Home/Home.js
@@ -1,7 +1,10 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoins } from '@fortawesome/free-solid-svg-icons'
 import Navbar from '../../components/NavBar'
-import './Home.css'
 import Footer from '../../components/Footer/Footer'
 import CoinSummaryPage from '../CoinSummary/CoinSummaryPage'
+import './Home.css'
 
 const Home = () => {
 	if (!localStorage.getItem('token')) {
@@ -12,6 +15,14 @@ const Home = () => {
 		<div className="homePg">
 			<Navbar />
 			<div className="pgContent">
+				<Container className="mb-5" id="homeHeader">
+					<Row>
+						<Col id="homeTitle">
+							<FontAwesomeIcon icon={faCoins} id="homeIcon" />
+							CryptoMaster
+						</Col>
+					</Row>
+				</Container>
 				<CoinSummaryPage />
 				<Footer />
 			</div>
